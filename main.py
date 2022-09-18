1ce5a467be6bd30246920c4f7e9440ffbf4ff582
russell <russelledwardwilson@gmail.com>
2022-09-18 11:11:53 +0100

    amended docker compose

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import naoqi
+import os
+from dotenv import load_dotenv
 
+# ENV variables 
+load_dotenv()
+IP = os.environ.get("IP")
+PORT = os.environ.get("PORT")
+
+
+print(IP)
 print('test env')
